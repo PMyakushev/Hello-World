a33c9995baaf8c4952cd8eef01eb81b427a3ae6e
Pavel <asercrut@yandex.ru>
2022-12-03 01:02:03 +0300

    New Example 5

---
+++ Example005_If_Else_Elif/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Add name Users");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Yeah, this is MASHA");
+}
+else
+{
+    Console.WriteLine("Hello, ");
+    Console.WriteLine(username);
+}
