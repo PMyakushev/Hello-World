0c27065c5f8e10c2572978f7959c50222962f196
Pavel <asercrut@yandex.ru>
2022-12-03 21:03:34 +0300

    WhileIndex

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array = { 1, 3, 3, 4, 5, 6, 7, 3, 9, 1};
+
+int n = array.Length; // возврат элементов массива
+int find = 5;
+int index = 0;
+
+while (index < n)
+{   
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;//прервать
+    }
+    //index = index + 1;
+    index++;
+}
